1b9d55d2a404039c257430bf043cdb4d89ebae89
Nabil Freeman <nabil@matchchat.co.uk>
2015-08-15 16:54:47 +0800

    bank

---
+++ public/assets/js/main.js
@@ -0,0 +1,111 @@
+var holder = window,
+
+//GOTTA MAKE SURE DRAG N DROP IS SUPPORTED
+tests = {
+  filereader: typeof FileReader != 'undefined',
+  dnd: 'draggable' in document.createElement('span'),
+  formdata: !!window.FormData,
+  progress: "upload" in new XMLHttpRequest
+};
+
+
+function makeTile(data){
+  var tile = document.createElement("div");
+  tile.className = "tile";
+  tile.innerHTML = '<div class="wrapper"></div>';
+
+  var img = document.createElement("div");
+  img.className = "thumbnail"
+
+  if (/(jpe?g|png|gif|svg)/g.test(data.mimetype)) {
+    img.setAttribute("style", "background-image:url(" + data.url + ");");
+  } else {
+    var extension = document.createElement("div");
+    extension.className = "extension circular-shadow";
+    extension.innerHTML = "FILE";
+    img.appendChild(extension);
+  }
+
+  tile.querySelector(".wrapper").appendChild(img);
+
+  var filename = document.createElement("div");
+  filename.className = "filename";
+  filename.innerHTML = data.filename;
+
+  tile.querySelector(".wrapper").appendChild(filename);
+
+  var tiles = document.querySelector(".tiles");
+  tiles.insertBefore(tile, tiles.querySelector(".tile"));
+}
+
+
+function readfiles(files) {
+  var formData = tests.formdata ? new FormData() : null;
+  for (var i = 0; i < files.length; i++) {
+    if (tests.formdata) formData.append('file', files[i]);
+  }
+
+  // now post a new XHR request
+  var xhr = new XMLHttpRequest();
+  xhr.open('POST', '/upload');
+  xhr.onload = function(e) {
+
+    if(!e.target.responseText){
+      console.log("ERRO ERROR ERRORO");
+      return;
+    }
+
+    var response = JSON.parse(e.target.responseText);
+
+    makeTile(response);
+
+  };
+
+  xhr.send(formData);
+}
+
+if (tests.dnd) {
+
+  holder.addEventListener("dragover", function(e){
+
+    console.log("DUDE! you're dragging!!!");
+
+    // holder.className = 'dragging';
+    e.stopPropagation();
+    e.preventDefault();
+  });
+
+  holder.addEventListener("dragend", function(e){
+    // holder.className = '';
+    e.preventDefault();
+  });
+
+  holder.addEventListener("drop", function(e){
+    // holder.className = '';
+    e.preventDefault();
+    readfiles(e.dataTransfer.files);
+  });
+
+}
+
+var init = function(){
+  var xhr = new XMLHttpRequest();
+
+  function load() {
+    if(this.responseText){
+      var rows = JSON.parse(this.responseText);
+
+      rows.forEach(function(row){
+        makeTile(JSON.parse(row.data));
+      });
+    }
+  }
+
+  var url = "http://localhost:8080/files";
+
+  xhr.addEventListener('load', load);
+  xhr.open("get", url, true);
+  xhr.send();
+};
+
+init();
--- server/server.js
+++ server/server.js
@@ -2,32 +2,120 @@ var express = require("express");
 var multer = require("multer");
 var mime = require('mime-types');
 var path = require('path');
+var mysql = require('mysql');
+var AWS = require('aws-sdk');
 
-var storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    cb(null, './uploads/')
-  },
-  filename: function (req, file, cb) {
-    cb(null, file.fieldname + '-' + Date.now() + "." + mime.extension(file.mimetype))
-  }
-})
+AWS.config.loadFromPath('./aws_config.json');
 
+var bucket = new AWS.S3({params: {Bucket: 'freeman-files'}});
+
+var db_config = {
+  host     : 'freeman-industries-1.cfxzsglbyoxx.us-east-1.rds.amazonaws.com',
+  database : 'bank',
+  multipleStatements : true,
+  user     : 'root',
+  password : 'c0mprom1sed',
+};
+
+function uploadToS3(file, destFileName, callback) {
+  bucket
+    .upload({
+      ACL: 'public-read',
+      Body: file.buffer,
+      Key: destFileName.toString() + "." + mime.extension(file.mimetype),
+      ContentType: file.mimetype // force download if it's accessed as a top location
+    })
+    .send(callback);
+}
+
+var getFiles = function(callback){
+  var connection = mysql.createConnection(db_config);
+  connection.connect();
+
+  connection.query('SELECT * FROM `files`', function(err, rows, fields) {
+    if (err) throw err;
+
+    callback(rows);
+
+    connection.end();
+  });
+}
+
+var storeFile = function(data_object){
+  var connection = mysql.createConnection(db_config);
+  connection.connect();
+
+  var query = "INSERT INTO `files` (`data`) VALUES (" + connection.escape(JSON.stringify(data_object)) + ");";
+
+  connection.query(query, function(err, rows, fields) {
+    if (err) throw err;
+
+    connection.end();
+  });
+}
+
+//for server storage
+
+// var storage = multer.diskStorage({
+//   destination: function (req, file, cb) {
+//     cb(null, './uploads/')
+//   },
+//   filename: function (req, file, cb) {
+//     console.log(file.fieldname);
+//     cb(null, file.fieldname + '-' + Date.now() + "." + mime.extension(file.mimetype))
+//   }
+// });
+//
 var upload = multer({
-  storage: storage
+  // storage: storage
 });
+var uploader = upload.array("file");
 
 var app = express(); //starts up your app
+
+app.use('/assets', express.static('public/assets'));
 
 app.get("/", function(req,res){
   res.sendFile('index.html', { root: path.join(__dirname, '../public') });
 });
 
-var uploader = upload.single("file");
-app.post("/upload", uploader, function(req,res){
-  res.send(req.file)
-  res.end();
+app.get("/files", function(req,res){
+  getFiles(function(rows){
+
+    if(rows){
+      res.send(JSON.stringify(rows))
+    } else {
+      res.send(null);
+    }
+
+    res.end();
+
+  });
 });
 
+app.post("/upload", uploader, function(req,res){
+
+  var pid = '10000' + parseInt(Math.random() * 10000000);
+
+  uploadToS3(req.files[0], pid, function (err, data) {
+    if (err) {
+      console.error(err);
+      return res.send(null).end();
+    }
+
+    var response_obj = {
+      filename: req.files[0].originalname,
+      mimetype: req.files[0].mimetype,
+      url: data.Location
+    };
+
+    res.send(JSON.stringify(response_obj)).end();
+
+    //async
+    storeFile(response_obj);
+
+  });
+});
 
 app.listen(8080);
 
